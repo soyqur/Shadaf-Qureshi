ffd8527334b9e2835b660d38fbd7f9e69194e09b
soyqur <soyqureshi@gmail.com>
2024-07-11 22:08:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showSidebar (){
+    const sidebar = document.querySelector ('.sidebar')
+    sidebar.style.display = ('flex');
+    
+
+}
+
+
+function hideSidebar (){
+    const hidesidebar = document.querySelector ('.sidebar')
+    hidesidebar.style.display = ('none')
+    
+}
+
